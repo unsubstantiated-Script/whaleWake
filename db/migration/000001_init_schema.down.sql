b492b58467dc58ed5eff5d406b3e8434082a1190
unsubstantiated-Script <jusfulton@gmail.com>
2024-12-28 14:40:33 -0800

    DB Setup Initial Migration
    Includes MakeFile commands.

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE if EXISTS user_role;
+DROP TABLE if EXISTS user_profile;
+DROP TABLE if EXISTS users;
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,46 @@
+CREATE TABLE "users" (
+                         "id" uuid PRIMARY KEY,
+                         "user_name" varchar NOT NULL,
+                         "email" varchar NOT NULL,
+                         "password" varchar NOT NULL,
+                         "created_at" timestamptz NOT NULL DEFAULT (now()),
+                         "updated_at" timestamptz NOT NULL DEFAULT (now()),
+                         "verified_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "user_role" (
+                             "id" uuid PRIMARY KEY,
+                             "user_id" uuid NOT NULL,
+                             "role_id" int NOT NULL,
+                             "created_at" timestamptz NOT NULL DEFAULT (now()),
+                             "updated_at" timestamptz NOT NULL DEFAULT (now()),
+                             "verified_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "user_profile" (
+                                "id" uuid PRIMARY KEY,
+                                "user_id" uuid NOT NULL,
+                                "first_name" varchar,
+                                "last_name" varchar,
+                                "business_name" varchar,
+                                "street_address" varchar,
+                                "city" varchar,
+                                "state" varchar,
+                                "zip" varchar,
+                                "country_code" int,
+                                "created_at" timestamptz NOT NULL DEFAULT (now()),
+                                "updated_at" timestamptz NOT NULL DEFAULT (now()),
+                                "verified_at" timestamptz DEFAULT (now())
+);
+
+CREATE INDEX ON "users" ("user_name");
+
+CREATE INDEX ON "user_role" ("user_id");
+
+CREATE INDEX ON "user_role" ("role_id");
+
+CREATE INDEX ON "user_profile" ("user_id");
+
+ALTER TABLE "user_role" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "user_profile" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
