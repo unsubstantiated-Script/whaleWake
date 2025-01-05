cc1bdb729c276642307e32ec5826c7488a624edb
unsubstantiated-Script <jusfulton@gmail.com>
2025-01-04 19:15:26 -0800

    Full CRUD Tests operational for the Users Table.

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,12 +2,14 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 	"whaleWake/util"
 )
 
-func TestCreateUser(t *testing.T) {
+func createRandomUser(t *testing.T) User {
 	arg := CreateUserParams{
 		UserName: util.RandomUserName(),
 		Email:    util.RandomEmail(),
@@ -26,10 +28,112 @@ func TestCreateUser(t *testing.T) {
 	require.NotZero(t, user.CreatedAt)
 	require.NotZero(t, user.UpdatedAt)
 
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	user := createRandomUser(t)
 	t.Cleanup(func() {
 		_, err := testQueries.DeleteUser(context.Background(), user.ID)
 		if err != nil {
 			return
 		}
 	})
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+
+	// Cleanup should be run before the require statements because if the require statements fail, the cleanup will not be run
+	t.Cleanup(func() {
+		_, err := testQueries.DeleteUser(context.Background(), user1.ID)
+		if err != nil {
+			return
+		}
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.UserName, user2.UserName)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.Password, user2.Password)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	arg := UpdateUserParams{
+		ID:       user1.ID,
+		UserName: user1.UserName,
+		Email:    util.RandomEmail(),
+		Password: user1.Password,
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	// Cleanup should be run before the require statements because if the require statements fail, the cleanup will not be run
+	t.Cleanup(func() {
+		_, err := testQueries.DeleteUser(context.Background(), user1.ID)
+		if err != nil {
+			return
+		}
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.UserName, user2.UserName)
+	// Because this is the one item getting updated, it relies upon the arg struct vs. the user1 struct
+	require.Equal(t, arg.Email, user2.Email)
+	require.Equal(t, user1.Password, user2.Password)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+}
+
+func TestDeleteUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	user1, err := testQueries.DeleteUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, user2)
+}
+
+func TestListUsers(t *testing.T) {
+	var userSlice []User
+
+	for i := 0; i < 10; i++ {
+		userSlice = append(userSlice, createRandomUser(t))
+	}
+
+	arg := ListUsersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	users, err := testQueries.ListUsers(context.Background(), arg)
+
+	t.Cleanup(func() {
+		for _, user := range userSlice {
+			_, err := testQueries.DeleteUser(context.Background(), user.ID)
+			if err != nil {
+				return
+			}
+		}
+	})
+
+	require.NoError(t, err)
+	require.Len(t, users, 5)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
 }
