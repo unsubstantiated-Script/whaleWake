1bbaecba118b23e30bdf61808703325b50ebe738
unsubstantiated-Script <jusfulton@gmail.com>
2025-07-23 19:17:52 -0700

    Bcrypt method/test setup

--- util/password_test.go
+++ util/password_test.go
@@ -1,1 +1,23 @@
 package util
+
+import (
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPasswordHash(password, hashedPassword)
+	require.NoError(t, err)
+
+	// Test with an incorrect password
+	incorrectPassword := RandomString(6)
+	err = CheckPasswordHash(incorrectPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
--- util/pwrd.go
+++ util/pwrd.go
@@ -1,1 +1,19 @@
 package util
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword hashes a plain-text password using bcrypt.
+// Returns the hashed password as a string, or an error if hashing fails.
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPasswordHash compares a plain-text password with a bcrypt hashed password.
+// Returns true if the password matches the hash, false otherwise.
+func CheckPasswordHash(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
