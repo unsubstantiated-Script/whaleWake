130979dd298a85355476c9294bc684e46c8e384b
unsubstantiated-Script <jusfulton@gmail.com>
2025-07-26 17:25:59 -0700

    Paseto Maker Interface

--- token/maker.go
+++ token/maker.go
@@ -1,1 +1,12 @@
 package token
+
+import "time"
+
+type Maker interface {
+	// CreateToken generates a new token for a specific user ID and duration.
+	CreateToken(username string, duration time.Duration) (string, error)
+	// VerifyToken checks the validity of a token and returns the user ID if valid.
+	VerifyToken(token string) (*Payload, error)
+	// RefreshToken generates a new token for a user ID based on an existing token.
+	RefreshToken(token string) (string, error)
+}
