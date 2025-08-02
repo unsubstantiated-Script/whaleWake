15c266b6677053b3c521c5a80ade4d4959906e57
unsubstantiated-Script <jusfulton@gmail.com>
2025-08-02 16:25:14 -0700

    More randomizing utils

--- util/random.go
+++ util/random.go
@@ -1,12 +1,15 @@
 package util
 
 import (
+	"encoding/hex"
+	"github.com/google/uuid"
 	"math/rand"
 	"strconv"
 	"strings"
 )
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const hexAlphabet = "0123456789abcdef"
 
 // RandomInt returns a random integer between min and max
 func RandomInt(min, max int64) int64 {
@@ -22,9 +25,33 @@ func RandomString(n int) string {
 	return string(b)
 }
 
+// RandomHexString returns a random hexadecimal string of length n
+func RandomHexString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = hexAlphabet[rand.Intn(len(hexAlphabet))]
+	}
+	return string(b)
+}
+
+// RandomSymmetricKey returns a random 32-byte symmetric key as a hex string
+func RandomSymmetricKey() string {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(key)
+}
+
 // RandomUserName  returns a random owner name
 func RandomUserName() string {
 	return RandomString(6)
+}
+
+// RandomUUID returns a random UUID string
+func RandomUUID() uuid.UUID {
+	return uuid.New()
 }
 
 // RandomBusinessName returns a random business name
